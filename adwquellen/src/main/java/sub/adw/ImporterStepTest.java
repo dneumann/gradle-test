f970f3c4ef413f0b9133ee798b84a3bfaeab0f6c
Dennis Neumann <neumann@sub.uni-goettingen.de>
2017-11-10 16:01:24 +0000

    Add a user defined step

---
+++ adwquellen/src/main/java/sub/adw/ImporterStepTest.java
@@ -0,0 +1,14 @@
+package sub.adw;
+
+import java.util.Map;
+
+import sub.ent.api.ImporterStep;
+
+public class ImporterStepTest extends ImporterStep {
+
+	@Override
+	public void execute(Map<String, String> params) {
+		out.println("    in test step");
+	}
+
+}
